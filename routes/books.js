c02a17eb2a06ddc5bb1f65001b368c1284ebf11d
atteekim <ssgod3370@gmail.com>
2016-03-04 09:08:00 +0900

    debug 수정

--- routes/books.js
+++ routes/books.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+// 책 검색
 router.get('/', function(req,res,next){
 
     var message ={
         "success" : {
             "message" : "1page에 대한 책 검색입니다.",
             "page":1,
-            "rows":30,
+            "rows":10,
             "searchList":[{
                 "isbn":"책id",
                 "title":"책제목",
@@ -53,6 +54,51 @@ router.get('/', function(req,res,next){
                 "category_id":"책 분류 번호",
                 "cover_url":"책표지의 파일 경로",
                 "pop_emotion_id":"책의 대표감정"
+            },{
+	            "isbn":"책id",
+	            "title":"책제목",
+	            "author":"저자",
+	            "publisher":"출판사",
+	            "total_page":"페이지",
+	            "category_id":"책 분류 번호",
+	            "cover_url":"책표지의 파일 경로",
+	            "pop_emotion_id":"책의 대표감정"
+            },{
+	            "isbn":"책id",
+	            "title":"책제목",
+	            "author":"저자",
+	            "publisher":"출판사",
+	            "total_page":"페이지",
+	            "category_id":"책 분류 번호",
+	            "cover_url":"책표지의 파일 경로",
+	            "pop_emotion_id":"책의 대표감정"
+            },{
+	            "isbn":"책id",
+	            "title":"책제목",
+	            "author":"저자",
+	            "publisher":"출판사",
+	            "total_page":"페이지",
+	            "category_id":"책 분류 번호",
+	            "cover_url":"책표지의 파일 경로",
+	            "pop_emotion_id":"책의 대표감정"
+            },{
+	            "isbn":"책id",
+	            "title":"책제목",
+	            "author":"저자",
+	            "publisher":"출판사",
+	            "total_page":"페이지",
+	            "category_id":"책 분류 번호",
+	            "cover_url":"책표지의 파일 경로",
+	            "pop_emotion_id":"책의 대표감정"
+            },{
+	            "isbn":"책id",
+	            "title":"책제목",
+	            "author":"저자",
+	            "publisher":"출판사",
+	            "total_page":"페이지",
+	            "category_id":"책 분류 번호",
+	            "cover_url":"책표지의 파일 경로",
+	            "pop_emotion_id":"책의 대표감정"
             }]
         }
     };
@@ -83,46 +129,120 @@ router.get('/:isbn/doodles', function(req,res,next){
 
     var message ={
         "success" : {
-            "message" : "1page의 나의 낙서기록이 조회되었습니다.",
-            "page":1,
+            "message" : "2page의 나의 낙서기록이 조회되었습니다.",
+            "page":2,
             "rows":10,
             "isbn":"책id",
             "doodleList":[{
-                "doodle_id": "낙서 id",
-                "date" : "날짜 ",
-                "page" : "페이지",
-                "text_doodle" : "글낙서",
-                "picture_doodle_url" : "사진낙서의 url",
-                "cover_url" : "책 표지의 파일 경로",
-                "emotion_doodle_id" : "감정낙서",
-                "heart": "낙서에 대한 추천"
+	            "user_photo_url" : "사용자의 사진",
+	            "nickname" : "별명",
+	            "doodle_id": "낙서 id",
+	            "doodle_time" : "날짜 ",
+	            "page" : "페이지",
+	            "text_doodle" : "글낙서",
+	            "picture_doodle_url" : "사진낙서의 url",
+	            "cover_url" : "책 표지의 파일 경로",
+	            "emotion_doodle_id" : "감정낙서",
+	            "heart": "낙서에 대한 추천"
             },{
-                "doodle_id": "낙서 id",
-                "date" : "날짜 ",
-                "page" : "페이지",
-                "text_doodle" : "글낙서",
-                "picture_doodle_url" : "사진낙서의 url",
-                "cover_url" : "책 표지의 파일 경로",
-                "emotion_doodle_id" : "감정낙서",
-                "heart": "낙서에 대한 추천"
+	            "user_photo_url" : "사용자의 사진",
+	            "nickname" : "별명",
+	            "doodle_id": "낙서 id",
+	            "doodle_time" : "날짜 ",
+	            "page" : "페이지",
+	            "text_doodle" : "글낙서",
+	            "picture_doodle_url" : "사진낙서의 url",
+	            "cover_url" : "책 표지의 파일 경로",
+	            "emotion_doodle_id" : "감정낙서",
+	            "heart": "낙서에 대한 추천"
             },{
-                "doodle_id": "낙서 id",
-                "date" : "날짜 ",
-                "page" : "페이지",
-                "text_doodle" : "글낙서",
-                "picture_doodle_url" : "사진낙서의 url",
-                "cover_url" : "책 표지의 파일 경로",
-                "emotion_doodle_id" : "감정낙서",
-                "heart": "낙서에 대한 추천"
+	            "user_photo_url" : "사용자의 사진",
+	            "nickname" : "별명",
+	            "doodle_id": "낙서 id",
+	            "doodle_time" : "날짜 ",
+	            "page" : "페이지",
+	            "text_doodle" : "글낙서",
+	            "picture_doodle_url" : "사진낙서의 url",
+	            "cover_url" : "책 표지의 파일 경로",
+	            "emotion_doodle_id" : "감정낙서",
+	            "heart": "낙서에 대한 추천"
             },{
-                "doodle_id": "낙서 id",
-                "date" : "날짜 ",
-                "page" : "페이지",
-                "text_doodle" : "글낙서",
-                "picture_doodle_url" : "사진낙서의 url",
-                "cover_url" : "책 표지의 파일 경로",
-                "emotion_doodle_id" : "감정낙서",
-                "heart": "낙서에 대한 추천"
+	            "user_photo_url" : "사용자의 사진",
+	            "nickname" : "별명",
+	            "doodle_id": "낙서 id",
+	            "doodle_time" : "날짜 ",
+	            "page" : "페이지",
+	            "text_doodle" : "글낙서",
+	            "picture_doodle_url" : "사진낙서의 url",
+	            "cover_url" : "책 표지의 파일 경로",
+	            "emotion_doodle_id" : "감정낙서",
+	            "heart": "낙서에 대한 추천"
+            },{
+	            "user_photo_url" : "사용자의 사진",
+	            "nickname" : "별명",
+	            "doodle_id": "낙서 id",
+	            "doodle_time" : "날짜 ",
+	            "page" : "페이지",
+	            "text_doodle" : "글낙서",
+	            "picture_doodle_url" : "사진낙서의 url",
+	            "cover_url" : "책 표지의 파일 경로",
+	            "emotion_doodle_id" : "감정낙서",
+	            "heart": "낙서에 대한 추천"
+            },{
+	            "user_photo_url" : "사용자의 사진",
+	            "nickname" : "별명",
+	            "doodle_id": "낙서 id",
+	            "doodle_time" : "날짜 ",
+	            "page" : "페이지",
+	            "text_doodle" : "글낙서",
+	            "picture_doodle_url" : "사진낙서의 url",
+	            "cover_url" : "책 표지의 파일 경로",
+	            "emotion_doodle_id" : "감정낙서",
+	            "heart": "낙서에 대한 추천"
+            },{
+	            "user_photo_url" : "사용자의 사진",
+	            "nickname" : "별명",
+	            "doodle_id": "낙서 id",
+	            "doodle_time" : "날짜 ",
+	            "page" : "페이지",
+	            "text_doodle" : "글낙서",
+	            "picture_doodle_url" : "사진낙서의 url",
+	            "cover_url" : "책 표지의 파일 경로",
+	            "emotion_doodle_id" : "감정낙서",
+	            "heart": "낙서에 대한 추천"
+            },{
+	            "user_photo_url" : "사용자의 사진",
+	            "nickname" : "별명",
+	            "doodle_id": "낙서 id",
+	            "doodle_time" : "날짜 ",
+	            "page" : "페이지",
+	            "text_doodle" : "글낙서",
+	            "picture_doodle_url" : "사진낙서의 url",
+	            "cover_url" : "책 표지의 파일 경로",
+	            "emotion_doodle_id" : "감정낙서",
+	            "heart": "낙서에 대한 추천"
+            },{
+	            "user_photo_url" : "사용자의 사진",
+	            "nickname" : "별명",
+	            "doodle_id": "낙서 id",
+	            "doodle_time" : "날짜 ",
+	            "page" : "페이지",
+	            "text_doodle" : "글낙서",
+	            "picture_doodle_url" : "사진낙서의 url",
+	            "cover_url" : "책 표지의 파일 경로",
+	            "emotion_doodle_id" : "감정낙서",
+	            "heart": "낙서에 대한 추천"
+            },{
+	            "user_photo_url" : "사용자의 사진",
+	            "nickname" : "별명",
+	            "doodle_id": "낙서 id",
+	            "doodle_time" : "날짜 ",
+	            "page" : "페이지",
+	            "text_doodle" : "글낙서",
+	            "picture_doodle_url" : "사진낙서의 url",
+	            "cover_url" : "책 표지의 파일 경로",
+	            "emotion_doodle_id" : "감정낙서",
+	            "heart": "낙서에 대한 추천"
             }]
         }
     };
