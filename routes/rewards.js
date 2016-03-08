43464aca9b985c45729ee7d8c08a35cb8d09ccae
im bitna <qlcsk0506@gmail.com>
2016-03-08 16:19:10 +0900

    리워드더미

--- routes/rewards.js
+++ routes/rewards.js
@@ -12,23 +12,22 @@ router.get('/me', function(req,res,next){
             "message" : "리워드 기록입니다.",
             "page":1,
             "rows":10,
-            "user_id" :"사용자id",
             "rewardList":[{
-                "reward_id": "리워드 id",
-                "reward_type" : "리워드타입",
-                "reward_quantity" : "리워드 수 ",
+                "reward_id": 1,
+                "reward_type" : 1,
+                "reward_quantity" : 1,
                 "reward_image_url" : "리워드 사진 url"
             },
                 {
-                    "reward_id": "리워드 id",
-                    "reward_type" : "리워드 타입",
-                    "reward_quantity" : "리워드 수 ",
+                    "reward_id": 2,
+                    "reward_type" : 1,
+                    "reward_quantity" : 10,
                     "reward_image_url" : "리워드 사진 url"
                 },
                 {
-                    "reward_id": "리워드 id",
-                    "reward_type" : "리워드타입",
-                    "reward_quantity" : "리워드 수 ",
+                    "reward_id": 3,
+                    "reward_type" : 1,
+                    "reward_quantity" : 20,
                     "reward_image_url" : "리워드 사진 url"
                 }
             ]
