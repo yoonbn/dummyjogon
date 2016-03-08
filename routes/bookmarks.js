e24a4b815207b5815029d3511c3315f21602fc79
atteekim <ssgod3370@gmail.com>
2016-03-08 16:15:14 +0900

    int

--- routes/bookmarks.js
+++ routes/bookmarks.js
@@ -10,7 +10,7 @@ router.get('/me', function(req,res,next){
     var message ={
         "success" : {
             "message" : "사용자의 월별 독서량 통계입니다.",
-            "month_page":"한 달 동안 읽은 페이지"
+            "month_page" : 88
         }
     };
     res.json(message);
