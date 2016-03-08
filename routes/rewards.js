789340cf997b341b8a0c0cef428c454a36106ede
atteekim <ssgod3370@gmail.com>
2016-03-08 17:33:45 +0900

    debug 수정

--- routes/rewards.js
+++ routes/rewards.js
@@ -10,26 +10,31 @@ router.get('/me', function(req,res,next){
     var message ={
         "success" : {
             "message" : "리워드 기록입니다.",
-            "page":1,
-            "rows":10,
+	        "pageNum" : 2,
             "rewardList":[{
-                "reward_id": 1,
-                "reward_type" : 1,
-                "reward_quantity" : 1,
-                "reward_image_url" : "리워드 사진 url"
-            },
+		            "reward_type" : 1,
+		            "reward_image_url" : "리워드 사진 url"
+                },
                 {
-                    "reward_id": 2,
                     "reward_type" : 1,
-                    "reward_quantity" : 10,
                     "reward_image_url" : "리워드 사진 url"
                 },
                 {
-                    "reward_id": 3,
                     "reward_type" : 1,
-                    "reward_quantity" : 20,
                     "reward_image_url" : "리워드 사진 url"
-                }
+                },{
+		            "reward_type" : 1,
+		            "reward_image_url" : "리워드 사진 url"
+	            },{
+		            "reward_type" : 1,
+		            "reward_image_url" : "리워드 사진 url"
+	            },{
+		            "reward_type" : 1,
+		            "reward_image_url" : "리워드 사진 url"
+	            },{
+		            "reward_type" : 1,
+		            "reward_image_url" : "리워드 사진 url"
+	            }
             ]
         }
     };
