d39666e94d92c5f0326ce9529e8383578a8709b6
atteekim <ssgod3370@gmail.com>
2016-03-08 16:18:20 +0900

    as

--- routes/recommendations.js
+++ routes/recommendations.js
@@ -10,24 +10,21 @@ router.get('/me', function(req,res,next){
 
     var message ={
         "success" : {
-            "message" : "1페이지의 추천입니다.",
-            "page":1,
-            "rows":3,
-            "recommend_id" :"추천id",
+            "message" : "추천입니다.",
+	        "pageNum" : 2,
             "recommendBook" : {
-	            "recommend_book_id": "추천책 id",
-	            "isbn" : "isbn",
+	            "isbn" : 9791186195482,
 	            "name" : "책제목",
-	            "emotion" : "책대표감정"
-                                },
+	            "emotion" : 3
+            },
             "recommendProgram": {
-	            "programId" : "프로그램 id",
+	            "programId" : 2,
 	            "programName" : "프로그램명",
 	            "programImage" : "프로그램 대표 이미지",
 	            "programContents" : "프로그램 내용"
                                 },
             "recommendClub" : {
-	            "clubId" : "모임 id",
+	            "clubId" : 2,
 	            "clubName" : "모임명",
 	            "clubImage" : "모임 대표 이미지",
 	            "clubContents": "모임내용"
