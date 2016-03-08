6494d4393a0114fa910a95f36d3894c9330c9fcc
atteekim <ssgod3370@gmail.com>
2016-03-08 17:04:48 +0900

    debug 수정

--- routes/bookcases.js
+++ routes/bookcases.js
@@ -14,7 +14,7 @@ router.get('/me', function(req,res,next){
 			"message" : "사용자의 책목록을 시제에 맞게 출력합니다.",
 			"pageNum" : 4,
 			"tenseList" : [{
-				"isbn" : 9788900389210,
+				"isbn" : "9788900389210",
 				"cover_url":"책표지의 파일 경로",
 				"title" : "책제목",
 				"author" : "저자",
@@ -23,7 +23,7 @@ router.get('/me', function(req,res,next){
 				"total_page" : 120,
 				"pop_emotion_id" : 1
 			},{
-				"isbn" : 9788901161198,
+				"isbn" : "9788901161198",
 				"cover_url" : "책표지의 파일 경로",
 				"title" : "책제목",
 				"author" : "저자",
@@ -32,7 +32,7 @@ router.get('/me', function(req,res,next){
 				"total_page" : 120,
 				"pop_emotion_id" : 2
 			},{
-				"isbn" : 9788901204772,
+				"isbn" : "9788901204772",
 				"cover_url" : "책표지의 파일 경로",
 				"title" : "책제목",
 				"author" : "저자",
@@ -41,7 +41,7 @@ router.get('/me', function(req,res,next){
 				"total_page" : 120,
 				"pop_emotion_id" : 5
 			},{
-				"isbn" : 9788936479671,
+				"isbn" : "9788936479671",
 				"cover_url" : "책표지의 파일 경로",
 				"title" : "책제목",
 				"author" : "저자",
@@ -50,7 +50,7 @@ router.get('/me', function(req,res,next){
 				"total_page" : 120,
 				"pop_emotion_id": 4
 			},{
-				"isbn" : 9788937839719,
+				"isbn" : "9788937839719",
 				"cover_url" : "책표지의 파일 경로",
 				"title" : "책제목",
 				"author" : "저자",
@@ -59,7 +59,7 @@ router.get('/me', function(req,res,next){
 				"total_page" : 120,
 				"pop_emotion_id" : 5
 			},{
-				"isbn" : 9788971997048,
+				"isbn" : "9788971997048",
 				"cover_url" : "책표지의 파일 경로",
 				"title" : "책제목",
 				"author" : "저자",
@@ -68,7 +68,7 @@ router.get('/me', function(req,res,next){
 				"total_page" : 120,
 				"pop_emotion_id" : 1
 			},{
-				"isbn" : "책id",
+				"isbn" : "9788971997048",
 				"cover_url" : "책표지의 파일 경로",
 				"title" : "책제목",
 				"author" : "저자",
@@ -77,7 +77,7 @@ router.get('/me', function(req,res,next){
 				"total_page" : 120,
 				"pop_emotion_id" : 3
 			},{
-				"isbn" : 9788971997116,
+				"isbn" : "9788971997116",
 				"cover_url" : "책표지의 파일 경로",
 				"title" : "책제목",
 				"author" : "저자",
@@ -86,7 +86,7 @@ router.get('/me', function(req,res,next){
 				"total_page" : 120,
 				"pop_emotion_id" : 2
 			},{
-				"isbn" : 9788972753698,
+				"isbn" : "9788972753698",
 				"cover_url" : "책표지의 파일 경로",
 				"title" : "책제목",
 				"author" : "저자",
@@ -95,7 +95,7 @@ router.get('/me', function(req,res,next){
 				"total_page" : 120,
 				"pop_emotion_id" : 4
 			},{
-				"isbn" : 9788972757573,
+				"isbn" : "9788972757573",
 				"cover_url" : "책표지의 파일 경로",
 				"title" : "책제목",
 				"author" : "저자",
